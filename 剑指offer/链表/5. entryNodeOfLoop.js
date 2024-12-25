26bc3df3b164a7ea1b76780b5c29c15aba4fda0b
wangqi01 <13693607080@163.com>
2024-12-25 11:29:14 +0800

    feat: add 5.3

--- 剑指offer/链表/5. entryNodeOfLoop.js
+++ 剑指offer/链表/5. entryNodeOfLoop.js
@@ -47,9 +47,37 @@ function EntryNodeOfLoop(pHead) {
     // 空间复杂度 O(1)，时间复杂度 O(n)
 
     // 解法3：快慢指针
-    // fast两倍，slow一倍，如果相遇则有环，入环点是
     let fast = pHead
     let slow = pHead
-
+    while (fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
+        if (fast === slow) {
+            break
+        }
+    }
+    if(!fast || !fast.next) return null // 有种情况是链表没环，也走到头了
+    slow = pHead
+    while (fast !==slow) {
+        fast = fast.next
+        slow = slow.next
+        if (fast === slow) {
+            break
+        }
+    }
+    return slow
     // 空间复杂度 O(1)，时间复杂度 O(n)
+
+    // fast两倍，slow一倍，fast进环转圈，链表有环必相遇（第一次相遇），
+    // 相遇后，fast从相遇点出发，改为一倍速，slow从链表头起点出发，相遇时即为入环点（第二次相遇）
+    // 第一次相遇时：
+    // 链表头到环入口长度为--a
+    // 环入口到相遇点长度为--b
+    // 相遇点到环入口长度为--c
+    // 快指针路程=a+(b+c)k+b ，k>=1  其中b+c为环的长度，k为绕环的圈数
+    // 慢指针路程=a+b
+    // 快指针走的路程是慢指针的两倍（a+b）*2=a+(b+c)k+b
+    // 化简得：a=(k-1)(b+c)+c,链表头到环入口的距离=相遇点到环入口的距离,最后一定相遇于环入口
+
+    // 也就是说，快指针从相遇点开始一倍速走到环入口时（c），慢指针从链表头开始一倍速走到环入口（a）
 }
